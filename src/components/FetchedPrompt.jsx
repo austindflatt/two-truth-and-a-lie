777fdd620fca61267dd2b44e6bec9e18572ae4f3
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-03-16 16:58:14 -0400

    fix: fixed indentation

--- src/components/FetchedPrompt.jsx
+++ src/components/FetchedPrompt.jsx
@@ -4,30 +4,31 @@ import React from 'react'
 export default function FetchedPrompt(props) {
   return (
 	  <>
-		<h1>From the server</h1>
-          <div>
-            <label>Fetched Username:</label>
-            {props.fetchedUsernameProp}
-            <br />
+	  <h1>From the server</h1>
+		<div>
+			<label>Fetched Username:</label>
+			{props.fetchedUsernameProp}
+			<br />
 
-            <label>Prompt 1:</label>
-            {props.fetchedPromptOneProp}
-            <br />
-            <label>Prompt 2:</label>
-            {props.fetchedPromptTwoProp}
-            <br />
-            <label>Prompt 3:</label>
-            {props.fetchedPromptThreeProp}
-            <br />
-        
-            <label>Vote 1:</label>
-            {props.fetchedVoteOneProp}
-            <label>Vote 2:</label>
-            {props.fetchedVoteTwoProp}
-            <label>Vote 3:</label>
-            {props.fetchedVoteThreeProp}
-          </div>
-		<button onClick={props.clickPoll}>Get Poll</button>
+			<label>Prompt 1:</label>
+			{props.fetchedPromptOneProp}
+			<br />
+			<label>Prompt 2:</label>
+			{props.fetchedPromptTwoProp}
+			<br />
+			<label>Prompt 3:</label>
+			{props.fetchedPromptThreeProp}
+			<br />
+
+			<label>Vote 1:</label>
+			{props.fetchedVoteOneProp}
+			<label>Vote 2:</label>
+			{props.fetchedVoteTwoProp}
+			<label>Vote 3:</label>
+			{props.fetchedVoteThreeProp}
+			<br />
+			<button onClick={props.clickPoll}>Get Poll</button>
+		</div>
 	</>
   )
 }
